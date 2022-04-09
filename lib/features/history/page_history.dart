4a19abb14a1c27013989aa047ea04aab2ee30ec7
Adhasetiawan <adhasetiawanwiyana@gmail.com>
2022-04-09 13:49:59 +0800

    List ui history has been fixed

--- lib/features/history/page_history.dart
+++ lib/features/history/page_history.dart
@@ -31,76 +31,80 @@ class PageHistory extends GetView<ControllerHistory> {
             child: Obx(
               () => controller.loading.isFalse
                   ? controller.allData.isNotEmpty
-                  ? ListView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          controller: controller.loadController,
-                          itemCount: controller.allData.length,
-                          itemBuilder: (context, index) {
-                            return Card(
-                              elevation: 4,
-                              color: AppColor.whiteColor,
-                              margin: const EdgeInsets.only(
-                                  top: 10, left: 10, right: 10),
-                              shape: const RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              child: Row(
-                                children: <Widget>[
-                                  Container(
-                                    margin: const EdgeInsets.only(
-                                        left: 15, top: 20, bottom: 20),
-                                    child: Icon(
-                                      CupertinoIcons.gobackward,
-                                      color: AppColor.primaryColor.shade600,
-                                      size: 35,
+                  ? SizedBox(
+                    height: Get.height - 120.h,
+                    width: Get.width,
+                    child: ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            controller: controller.loadController,
+                            itemCount: controller.allData.length,
+                            itemBuilder: (context, index) {
+                              return Card(
+                                elevation: 4,
+                                color: AppColor.whiteColor,
+                                margin: const EdgeInsets.only(
+                                    top: 10, left: 10, right: 10),
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                child: Row(
+                                  children: <Widget>[
+                                    Container(
+                                      margin: const EdgeInsets.only(
+                                          left: 15, top: 20, bottom: 20),
+                                      child: Icon(
+                                        CupertinoIcons.gobackward,
+                                        color: AppColor.primaryColor.shade600,
+                                        size: 35,
+                                      ),
                                     ),
-                                  ),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Container(
-                                        margin: const EdgeInsets.only(
-                                            left: 10, top: 5),
-                                        child: SizedBox(
-                                          width: Get.width * 0.77,
-                                          child: Text(
-                                            '${controller.allData[index].post?.cityOrigin} - ${controller.allData[index].post?.cityDestination}',
-                                            style: const TextStyle(
-                                                fontSize: 16,
-                                                overflow:
-                                                    TextOverflow.ellipsis),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Container(
+                                          margin: const EdgeInsets.only(
+                                              left: 10, top: 5),
+                                          child: SizedBox(
+                                            width: Get.width * 0.77,
+                                            child: Text(
+                                              '${controller.allData[index].post?.cityOrigin} - ${controller.allData[index].post?.cityDestination}',
+                                              style: const TextStyle(
+                                                  fontSize: 16,
+                                                  overflow:
+                                                      TextOverflow.ellipsis),
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                      Container(
-                                        margin: const EdgeInsets.only(
-                                            left: 10, top: 5),
-                                        child: Text(
-                                          DateFormat('dd-MM-yyyy').format(
-                                              controller
-                                                  .allData[index].createdAt!),
-                                          style: const TextStyle(fontSize: 14),
+                                        Container(
+                                          margin: const EdgeInsets.only(
+                                              left: 10, top: 5),
+                                          child: Text(
+                                            DateFormat('dd-MM-yyyy').format(
+                                                controller
+                                                    .allData[index].createdAt!),
+                                            style: const TextStyle(fontSize: 14),
+                                          ),
                                         ),
-                                      ),
-                                      Container(
-                                        margin: const EdgeInsets.only(
-                                            left: 10, top: 5, bottom: 5),
-                                        child: Text(
-                                          controller.allData[index].status == 4
-                                              ? 'Batalkan'
-                                              : 'Selesai',
-                                          style: const TextStyle(
-                                              fontSize: 14, color: Colors.red),
+                                        Container(
+                                          margin: const EdgeInsets.only(
+                                              left: 10, top: 5, bottom: 5),
+                                          child: Text(
+                                            controller.allData[index].status == 4
+                                                ? 'Batalkan'
+                                                : 'Selesai',
+                                            style: const TextStyle(
+                                                fontSize: 14, color: Colors.red),
+                                          ),
                                         ),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            );
-                          })
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              );
+                            }),
+                  )
                   : SizedBox(
                           height: Get.height - 85.h,
                           width: Get.width,
